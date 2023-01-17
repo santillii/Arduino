4cb42290b2038680ee25e82c7a942d8cf494d262
Luca Massimo Santilli <122351493+santillii@users.noreply.github.com>
2023-01-17 09:49:12 +0100

    Add files via upload

--- DHT11___MAX6675.ino
+++ DHT11___MAX6675.ino
@@ -41,30 +41,33 @@ void setup() {
   Serial.begin(9600);
   // Initialize device.
   dht.begin();
-  Serial.println(F("DHTxx Unified Sensor Example"));
+  //Serial.println(F("DHTxx Unified Sensor Example"));
   // Print temperature sensor details.
   sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
+  // QUESTA dht.temperature().getSensor(&sensor);
+  //Serial.println(F("------------------------------------"));
+  //Serial.println(F("Temperature Sensor"));
+  //Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  //Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  //Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  //Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  //Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  //Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  //Serial.println(F("------------------------------------"));
   // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
-  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
-  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
+  // QUESTA dht.humidity().getSensor(&sensor);
+  //Serial.println(F("Humidity Sensor"));
+  //Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  //Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  //Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  //Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  //Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  //Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  //Serial.println(F("------------------------------------"));
   // Set delay between sensor readings based on sensor details.
+
+  //intestazione colonne per file csv
+  Serial.println("Temperatura DHT 11 (°C), Umidità DHT 11 (%), Temperatura MAX6675 (°C)");
   delayMS = sensor.min_delay / 1000;
 }
 
@@ -78,9 +81,10 @@ void loop() {
     Serial.println(F("Error reading temperature!"));
   }
   else {
-    Serial.print(F("Temperature: "));
+    //Serial.print(F("Temperature: "));
     Serial.print(event.temperature);
-    Serial.println(F("°C"));
+    Serial.print(" , ");
+    //Serial.println(F("°C"));
   }
   // Get humidity event and print its value.
   dht.humidity().getEvent(&event);
@@ -88,12 +92,15 @@ void loop() {
     Serial.println(F("Error reading humidity!"));
   }
   else {
-    Serial.print(F("Humidity: "));
+    //Serial.print(F("Humidity: "));
     Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
+    Serial.print(" , ");
+    //Serial.println(F("%"));
   }
 
   //Loop MAX6675
-    Serial.print("MAX6675 temp= "); Serial.print(thermocouple.readCelsius()); Serial.println("°C");
+    //Serial.print("MAX6675 temp= "); 
+    Serial.print(thermocouple.readCelsius()); 
+    //Serial.println("°C");
     Serial.println(); //print newline
 }
